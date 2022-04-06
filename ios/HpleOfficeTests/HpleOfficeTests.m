b172e5cb2ea6e068acfece94467fbf8458fc95f5
thoanv <vanthoa225@gmail.com>
2022-04-06 16:24:26 +0700

    Thay đổi tên trong IOS

--- ios/HpleOfficeTests/HpleOfficeTests.m
+++ ios/HpleOfficeTests/HpleOfficeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HplSignTests : XCTestCase
+@interface HpleOfficeTests : XCTestCase
 
 @end
 
-@implementation HplSignTests
+@implementation HpleOfficeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
