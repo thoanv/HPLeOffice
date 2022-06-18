19aab0c03cd02bea8c7b95db31916ae2898589ff
thoanv <vanthoa225@gmail.com>
2022-06-18 12:07:41 +0700

    Commit code notification

---
+++ screens/ultils/helperFunctions.js
@@ -0,0 +1,17 @@
+import { showMessage } from 'react-native-flash-message';
+
+const showError = (message) => {
+    showMessage({
+        type: 'danger',
+        icon: 'danger',
+        message
+    })
+}
+
+const showSuccess = (message) => {
+    showMessage({
+        type: 'success',
+        icon: 'success',
+        message
+    })
+}
